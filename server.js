adb58868c9454ca961995013d63d909766415b43
lowlyphe <jones.kyle2893@gmail.com>
2022-09-12 12:34:21 -0400

    trying to fix cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/", router);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://kyle-jones.dev");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
