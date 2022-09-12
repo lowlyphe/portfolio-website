f63cd277c32a7f43747f3c667c6934379163eaa5
lowlyphe <jones.kyle2893@gmail.com>
2022-09-12 15:33:47 -0400

    fixing cors

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -8,23 +8,6 @@ export default function Technologies() {
         <p className='text-white'>
             I build applications with:&nbsp; 
         </p>
-        <Typist>
-            <span className='text-react'>React</span>
-            <Typist.Backspace count={5} delay={500} />
-            <span className='text-node'>Node</span>
-            <Typist.Backspace count={4} delay={500} />
-            <span className='text-psql'>PostgreSQL</span>
-            <Typist.Backspace count={10} delay={500} />
-            <span className='text-tailwind'>TailwindCSS</span>
-            <Typist.Backspace count={11} delay={500} />
-            <span className='text-red-500'>Responsive Layouts</span>
-            <Typist.Backspace count={18} delay={500} />
-            <span className='text-js'>Javascript</span>
-            <Typist.Backspace count={10} delay={500} />
-            <span className='text-node'>Express</span>
-            <Typist.Backspace count={7} delay={500} />
-            <span className='text-white'>You in mind</span>
-        </Typist>
     </div>
     
     
