5338e300bd722f1d120199c1150dd1a59b218c27
Kyle Jones <kyle.jones2893@gmail.com>
2023-10-30 15:07:30 -0400

    changed project stucture. Added new project

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import getProjects from '../data/projects'
+import Project from './Project'
 
 export default function ProjectBox() {
+
+  const projects = getProjects()
+
   return (
-    null
+    <div className='flex flex-col items-center'>
+      <h2 className='text-purple mb-10'>Featured Projects</h2>
+      <div className='flex flex-col items-center md:grid grid-cols-3 min-w-1/3 w-2/3'>
+        {projects.map((project) => <Project project={project}/>)}
+        </div>
+    </div>
   )
 }
